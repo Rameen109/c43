f70de65009ee1200d64b06bd93a1170a1d410757
Rameen109 <77060389+Rameen109@users.noreply.github.com>
2021-07-31 11:06:58 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    function clues (){
+        fill ("white");
+        textSize(15);
+        text ("R E V B A I L A",100,50);
+        //variable
+        fill ("lightblue");
+        text ("Hint : Always Changing, Not Constant !",100,70);
+        
+        fill ("white");
+        textSize(15);
+        text ("C U T N I F O N",200,100);
+        //function
+        fill ("lightblue");
+        text ("Hint : Perform A Perticular Task !!",200,120);
+        
+        fill ("white");
+        textSize(15);
+        text ("A T E D A S B A",100,150);
+        //DATABASE
+        fill ("lightblue");
+        text ("Hint : Stores All Information!! ",200,140);
+        
+        
+        }
+}
